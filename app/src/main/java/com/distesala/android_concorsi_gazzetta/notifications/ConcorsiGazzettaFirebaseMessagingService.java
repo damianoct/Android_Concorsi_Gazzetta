759a166fe6aa8039836cfa03e02395240ebff827
Damiano <damiano.di.stefano@gmail.com>
2017-05-25 21:05:45 +0200

    notifications received

--- app/src/main/java/com/distesala/android_concorsi_gazzetta/notifications/ConcorsiGazzettaFirebaseMessagingService.java
+++ app/src/main/java/com/distesala/android_concorsi_gazzetta/notifications/ConcorsiGazzettaFirebaseMessagingService.java
@@ -4,17 +4,60 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static com.google.android.gms.internal.zzt.TAG;
 
 /**
  * Created by damiano on 08/05/17.
  */
 
-public class ConcorsiGazzettaFirebaseMessagingService extends Service
+public class ConcorsiGazzettaFirebaseMessagingService extends FirebaseMessagingService
 {
-    @Nullable
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    // [START receive_message]
     @Override
-    public IBinder onBind(Intent intent)
-    {
-        return null;
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                //scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                //handleNow();
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
     }
+    // [END receive_message]
 }
